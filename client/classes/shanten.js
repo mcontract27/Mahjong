fad5b669b48f4c536bf9f55844fdd1da6f517a01
Matthew Contract <mcontract27@gmail.com>
2018-06-14 20:10:43 -0400

    shanten calculation working but slow

--- client/classes/shanten.js
+++ client/classes/shanten.js
@@ -9,63 +9,80 @@ const suitValues = {
 
 const copyHand = obj => {
   const newObject = {}
-  return Object.assign(newObject, obj)
+  Object.assign(newObject, obj)
+  return newObject
 }
 
 const removeTiles = (obj, arr, count = 1) => {
-    console.log(obj, arr)
   arr.forEach(tile => {
     obj[[tile[0], tile[1]]] -= count
     if (obj[[tile[0], tile[1]]] === 0) delete obj[[tile[0], tile[1]]]
   })
-  console.log(obj,arr)
 }
 
-const calculateShanten = (handHash, suitPos = 0, valuePos = 0) => {
-  //don't forget chiitoitsu/kokushi
+//don't forget chiitoitsu/kokushi
+const calcShanten = (
+  handHash,
+  threes = 0,
+  twos = 0
+) => {
   let shantenMin = 13
-  for (let i = suitPos; i < suits.length; i++) {
+  for (let i = 0; i < suits.length; i++) {
     let suit = suits[i]
-    for (let j = valuePos; j < suitValues[suit].length; j++) {
+    for (let j = 0; j < suitValues[suit].length; j++) {
       let value = suitValues[suit][j]
-      if (!handHash[[suit, value]]) continue;
+      if (!handHash[[suit, value]]) continue
       if (handHash[[suit, value]] >= 3) {
         let handCopy = copyHand(handHash)
         removeTiles(handCopy, [[suit, value]], 3)
-        shantenMin = Math.min(shantenMin, calculateShanten(handCopy, i, j))
+        shantenMin = Math.min(
+          shantenMin,
+          calcShanten(handCopy, threes + 1, twos)
+        )
       }
-      if (handHash[[suit, value]] >= 2) {
+      if (i < 3 && handHash[[suit, value + 1]] && handHash[[suit, value + 2]]) {
+        let handCopy = copyHand(handHash)
+        removeTiles(handCopy, [
+          [suit, value],
+          [suit, value + 1],
+          [suit, value + 2]
+        ])
+        shantenMin = Math.min(
+          shantenMin,
+          calcShanten(handCopy, threes + 1, twos)
+        )
+      }
+      if (i < 3 && threes + twos < 4 && handHash[[suit, value + 1]]) {
+        let handCopy = copyHand(handHash)
+        removeTiles(handCopy, [[suit, value], [suit, value + 1]])
+        shantenMin = Math.min(
+          shantenMin,
+          calcShanten(handCopy, threes, twos + 1)
+        )
+      }
+      if (i < 3 && threes + twos < 4 && handHash[[suit, value + 2]]) {
+        let handCopy = copyHand(handHash)
+        removeTiles(handCopy, [[suit, value], [suit, value + 2]])
+        shantenMin = Math.min(
+          shantenMin,
+          calcShanten(handCopy, threes, twos + 1)
+        )
+      }
+      if (threes + twos < 4 && handHash[[suit, value]] >= 2) {
         let handCopy = copyHand(handHash)
         removeTiles(handCopy, [[suit, value]], 2)
-        shantenMin = Math.min(shantenMin, calculateShanten(handCopy, i, j))
-      }
-      if (i < 3) {
-        if (handHash[[suit, value + 1]] && handHash[[suit, value + 2]]) {
-          let handCopy = copyHand(handHash)
-          removeTiles(handCopy, [
-            [suit, value],
-            [suit, value + 1],
-            [suit, value + 2]
-          ])
-          shantenMin = Math.min(shantenMin, calculateShanten(handCopy, i, j))
-        }
-        if (handHash[[suit, value + 1]]) {
-          let handCopy = copyHand(handHash)
-          removeTiles(handCopy, [[suit, value], [suit, value + 1]])
-          shantenMin = Math.min(shantenMin, calculateShanten(handCopy, i, j))
-        }
-        if (handHash[[suit, value + 2]]) {
-          let handCopy = copyHand(handHash)
-          removeTiles(handCopy, [[suit, value], [suit, value + 2]])
-          shantenMin = Math.min(shantenMin, calculateShanten(handCopy, i, j))
-        }
+        shantenMin = Math.min(
+          shantenMin,
+          calcShanten(handCopy, threes, twos + 1)
+        )
       }
     }
   }
   const values = Object.values(handHash)
-  let sum = values.reduce((a, b) => a + b, 0)
-  if (sum > values.length) sum -= 2
-  return Math.min(sum, shantenMin)
+  const sum = values.reduce((a, b) => +a + +b, 0)
+  let shanten = twos + (sum - (twos + 1)) * 2 / 3
+  if (sum > values.length) shanten--
+  return Math.min(shanten, shantenMin)
 }
 
 export const getShanten = hand => {
@@ -75,11 +92,10 @@ export const getShanten = hand => {
     let count = handHash[[tile.suit, tile.value]] || 0
     handHash[[tile.suit, tile.value]] = count + 1
   })
-  hand.forEach(tile => {
+  Object.keys(handHash).forEach(tile => {
     let handCopy = copyHand(handHash)
-    removeTiles(handCopy, [[tile.suit, tile.value]])
-    // console.log(tile, obj)
-    shantens[[tile.suit, tile.value]] = calculateShanten(handCopy)
+    removeTiles(handCopy, [tile.split(',')])
+    shantens[tile] = calcShanten(handCopy)
   })
   console.log(shantens)
 }
